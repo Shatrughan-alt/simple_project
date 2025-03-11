32e4ade4ecfbca5eb28996a1f3b9b3f58dff7989
Shatrughan-alt <bisenprateeksingh@gmail.com>
2025-03-11 11:28:27 +0530

    Commit

---
+++ src/com/Main.java
@@ -0,0 +1,12 @@
+package com;
+
+import com.model.Book;
+
+public class Main {
+	public static void main(String args[]) {
+		Book b=new Book();
+		b.setTitle("Shatrughan title");
+		b.setAuthor("Shatrughan");
+		System.out.println(b);
+	}
+}
