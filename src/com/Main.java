2a5874d9792b4d350c0898c1680d1062e69f93ff
Shatrughan-alt <bisenprateeksingh@gmail.com>
2025-03-11 11:49:19 +0530

    Second

--- src/com/Main.java
+++ src/com/Main.java
@@ -1,12 +1,16 @@
 package com;
 
+import com.model.Author;
 import com.model.Book;
 
 public class Main {
 	public static void main(String args[]) {
 		Book b=new Book();
 		b.setTitle("Shatrughan title");
-		b.setAuthor("Shatrughan");
+		Author auth=new Author();
+		auth.setName("Auth");
+		auth.setAuthor("Shatrughan");
+		 b.setAuthor(auth);
 		System.out.println(b);
 	}
 }
